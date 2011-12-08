a530e3cdebf85a2fb959b5e4eb6bd31d5b7cb24e
Rob Carlson <rob@vees.net>
2011-12-07 21:00:18 -0500

    Minor cleanup, add to ignore for pyc

--- bcd_status.py
+++ bcd_status.py
@@ -7,8 +7,18 @@ ser.open()
 
 ser.write("STS\r")
 time.sleep(.25)
-print ser.inWaiting()
-print ser.read(ser.inWaiting())
+#print ser.inWaiting()
+bcdreply = ser.read(ser.inWaiting())
+
+bcdparts=bcdreply.split(',')
+print bcdparts[4]
+#ascii char 130 is a down arrow on the screen
+print bcdparts[6].replace(chr(130),'v')
+#for cr in bcdparts[6]:
+#	print ord(cr)
+print bcdparts[8]
+print bcdparts[10]
+print bcdparts[12]
 
 ser.close()
 
--- serinit.py
+++
@@ -1,4 +0,0 @@
-import serial
-ser=serial.Serial('/dev/ttyUSB0',115200, rtscts=0)
-ser.open()
-

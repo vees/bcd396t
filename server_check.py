e545b4cc5410928bda6fa146b97ae2b092f06c6b
Rob Carlson <rob@vees.net>
2011-12-08 15:48:38 -0500

    Python client and PHP server first draft

--- server_check.py
+++ server_check.py
@@ -1,7 +1,18 @@
 from Bcd396tSerial import *
+import urllib2
+import urllib
 
 bs=Bcd396tSerial()
-bs.set_quick_by_id('5285','1222221220')
 #bs.set_quick_by_id('5285','1111111110')
-print bs.status_text()
+statustext = bs.status_text()
+print statustext
+
+request = urllib2.Request(
+	"https://vees.net/scanner/index.php",
+	urllib.urlencode({"status": statustext, "foo":"bar"})
+)
+reply = urllib2.urlopen(request)
+
+quickset = reply.read(10)
+bs.set_quick_by_id('5285',quickset)
 

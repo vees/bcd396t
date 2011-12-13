65ce9b6963f700666b572516438ab2d01bbed020
Rob Carlson <rob@vees.net>
2011-12-13 11:05:13 -0500

    Scheduled client/scanner interface for first prototype

--- server_check.py
+++ server_check.py
@@ -7,12 +7,19 @@ bs=Bcd396tSerial()
 statustext = bs.status_text()
 print statustext
 
-request = urllib2.Request(
-	"https://vees.net/scanner/index.php",
-	urllib.urlencode({"status": statustext, "foo":"bar"})
+url = "https://vees.net/scanner/index.php"
+
+request = urllib2.Request(url,
+	urllib.urlencode({"status": statustext})
 )
 reply = urllib2.urlopen(request)
 
 quickset = reply.read(10)
 bs.set_quick_by_id('5285',quickset)
+
+statustext = bs.status_text()
+request = urllib2.Request(url,
+        urllib.urlencode({"status": statustext})
+)
+
 
